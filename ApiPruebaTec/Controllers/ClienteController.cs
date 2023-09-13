f34b536def4830e2a22d261fed5a9b6b35c1cfd7
Rut <rut.luye@gmail.com>
2023-09-13 14:02:51 -0500

    primer commit

---
+++ ApiPruebaTec/Controllers/ClienteController.cs
@@ -0,0 +1,65 @@
+﻿using ApiPruebaTec.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiPruebaTec.Controllers
+{
+    [ApiController]
+    [Route("cliente")]
+
+    public class ClienteController : ControllerBase
+    {
+        [HttpGet]
+        [Route("listar")]
+        public dynamic ListarCliente()
+        {
+
+            List<Cliente> cliente = new List<Cliente>
+            {
+
+            new Cliente
+            {
+                id = 1,
+                nombre = "Rut Luye",
+                edad = 30,
+                correo = "rutluye@gmail.com"
+
+            },
+            new Cliente
+            {
+
+                id = 2,
+                nombre = "Ariana Luye",
+                edad = 20,
+                correo = "rutluye@gmail.com"
+
+            }
+            };
+
+
+            return cliente;
+
+        }
+        [HttpPost]
+        [Route("guardar")]
+
+        public dynamic GuardarCliente(Cliente cliente) 
+        
+        { 
+
+            cliente.id = 1;
+
+            return new
+            {
+
+                success = true,
+                message = "Cliente registrado",
+                result = cliente
+
+            };
+        
+        }
+
+
+
+    }
+    }
---
+++ ApiPruebaTec/Models/Cliente.cs
@@ -0,0 +1,14 @@
+﻿namespace ApiPruebaTec.Models
+{
+    public class Cliente
+    {
+
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public int edad { get; set; }
+
+        public string correo { get; set; }
+
+
+    }
+}
